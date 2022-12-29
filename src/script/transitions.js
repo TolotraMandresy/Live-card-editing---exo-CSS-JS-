dee2d6e08e7b403f73335fc7ed6e6e2eb7703d3b
Tolotra <tolotramandresy39@gmail.com>
2022-12-29 14:09:44 +0300

    nearly all done exept month validation

--- src/script/transitions.js
+++ src/script/transitions.js
@@ -1,53 +1,56 @@
-const  cardsAnimation= (function (){
-    const cardsContainer= document.getElementById('cartsContainer');
-    const transitionDuration= {
+const cardsAnimation = (function () {
+    const cardsContainer = document.getElementById('cartsContainer');
+    const transitionDuration = {
         lateral: parseInt(getComputedStyle(document.querySelector(':root')).getPropertyValue('--lateral-transition-duration')),
         vertical: parseInt(getComputedStyle(document.querySelector(':root')).getPropertyValue('--vertical-transition-duration'))
     };
-    let cardsReversed= false;
-    
-    const transitions= [ 
-        async () =>  {
-            //move cards laterally
-            await delay(()=>{
-                cardsContainer.classList.toggle('moveLaterally');
-            }, transitionDuration.lateral);
-        },
-        async () =>  {
-            //move cards vertically
-            await delay(()=>{
-                cardsContainer.classList.toggle('moveVertically');
-            }, transitionDuration.vertical);
-        },
-        async () =>  {
-            await delay(()=>{
-                cardsContainer.classList.toggle('revertCards');
-            }, transitionDuration.lateral);
-        }
-    ]
+    let cardsReversed = false;
 
-    function delay(todo, delayTime){
-        return new Promise(( resolve )=>{
+    const delay = (todo, delayTime) => {
+        return new Promise((resolve) => {
             todo()
             setTimeout(resolve, delayTime);
         })
     }
 
-    const animate= async function(reverse= false){
-        if(cardsReversed != reverse){
-            const currentTransition= (cardsReversed) ? [...transitions].reverse() : transitions;
-            for(let t of currentTransition){
-                await t();
-            }
-            cardsReversed= !cardsReversed; 
+    const transitions = [
+        async () => {
+            //move cards laterally
+            await delay(() => {
+                cardsContainer.classList.toggle('moveLaterally');
+            }, transitionDuration.lateral);
+        },
+        async () => {
+            //move cards vertically
+            await delay(() => {
+                cardsContainer.classList.toggle('moveVertically');
+            }, transitionDuration.vertical);
+        },
+        async () => {
+            await delay(() => {
+                cardsContainer.classList.toggle('revertCards');
+            }, transitionDuration.lateral);
         }
+    ]
 
+    return {
+        animate: async function (reverse = false) {
+            if (cardsReversed != reverse) {
+                const currentTransition = (cardsReversed) ? [...transitions].reverse() : transitions;
+                for (let t of currentTransition) {
+                    await t();
+                }
+                cardsReversed = !cardsReversed;
+            }
+
+        }
     }
-
-    return { animate }
 })()
 
-function toggleSuccessMessage(){
-    const  container= document.getElementById('formAndMsg');
-    container.classList.toggle('showMessage');
+function toggleSuccessMessage(formIsValid= true) {
+    if(formIsValid){
+        const container = document.getElementById('formAndMsg');
+        container.classList.toggle('showMessage');
+        document.getElementsByTagName('form')[0].reset();
+    }
 }
--- src/script/validation.js
+++ src/script/validation.js
@@ -1,43 +1,131 @@
-const validation= (()=>{
-    function validate(formControlId, regExp, msg){
-        const inputs= document.querySelectorAll(`#${formControlId} input`)
-        const errorMsgElement= document.querySelector(`#${formControlId} small`)
-        let isValid= true
+const liveEditing = (() => {
+    String.prototype.capitalize = function () {
+        return this.split(' ').map(t => (t[0]) ? t[0].toUpperCase() + t.substring(1) : '').join(' ')
+    }
 
-        for (let i=0; i<inputs.length; i++) {
-            console.log(inputs[i].value);
-            isValid= regExp.test(inputs[i].value) 
-            const background= document.querySelector(`#${formControlId} .gradient:nth-of-type(${i+1})`)
-            if(isValid){
-                errorMsgElement.innerText= ""
-                background.classList.remove('error')
+    class FormControl {
+        static verify(formControl, regExp, msg) {
+            const inputs = document.querySelectorAll(`#${formControl.id} input`)
+            const errorMsgElement = document.querySelector(`#${formControl.id} small`)
+            let isValid = true
+
+            for (let i = 0; i < inputs.length; i++) {
+                const background = document.querySelector(`#${formControl.id} .gradient:nth-of-type(${i + 1})`)
+                const value = inputs[i].value
+
+                isValid = isValid && regExp.test(value)
+                formControl.value[i] = value
+
+                if (!isValid) {
+                    errorMsgElement.innerText = msg
+                    background.classList.add('error')
+                    break
+                }
+                else {
+                    errorMsgElement.innerText = ""
+                    background.classList.remove('error')
+                }
             }
-            else{
-                errorMsgElement.innerText= msg
-                background.classList.add('error')
+            
+            return isValid
+        }
 
-                break
+        static containAlphaOnly(formControl) {
+            return function () {
+                return FormControl.verify(formControl, /^[A-Za-z]+(?: [A-Za-z]+)*$/, 'Wrong format, alphabetic only')
             }
         }
-    }
-    
-    function containAlphaOnly(id){
-        validate(id, /^[A-Za-z]+(?: [A-Za-z])*$/, 'Wrong format, alphabetic only')
-    }
-    
-    function containNumOnly(id, quantificateur= '+'){
-        const regExp= new RegExp(`\\d${quantificateur}`)
-        validate(id, regExp, 'Wrong format, number only')
+
+        static containNumOnly(formControl, quantificateur = '+') {
+            // const minLength=  quantificateur.match(/\d+/)[0] 
+            const regExp = new RegExp(`^\\d${quantificateur}$`)
+            return function () {
+                return FormControl.verify(formControl, regExp, `Wrong format, number only`)
+                // return FormControl.verify(formControl, regExp, `Wrong format, ${minLength} digits number only`)
+            }
+        }
+
+        static isNotEmpty(formControl) {
+            return function () {
+                return FormControl.verify(formControl, /^.+$/, 'Can\'t be blank')
+            }
+        }
+
+        constructor(defaultVal, formControl, displayer, cantBeEmpty, alphaOnly, numOnly, options= {quantifNumOnly: '+', lengthFormat: 0, separator: ''}) {
+            this.defaultVal= defaultVal
+            this.id = formControl
+            this.displayer = displayer
+            this.value = ['']
+            this.verification = []
+            this.separator = options.separator
+            this.lengthFormat = options.lengthFormat
+
+            if (cantBeEmpty) this.verification.push(FormControl.isNotEmpty(this))
+            if (alphaOnly) this.verification.push(FormControl.containAlphaOnly(this))
+            if (numOnly) this.verification.push(FormControl.containNumOnly(this, options.quantifNumOnly))
+        }
+
+        validate() {
+            let valid= true
+            for (const verif of this.verification){
+                valid= valid && verif()
+            }
+            return valid
+        }
+
+        formatValue() {
+            let splittedVal = this.value.map(val => val.capitalize()).join('')
+
+            //display deault if no value
+            if(!splittedVal) splittedVal= this.defaultVal.join('')
+            
+            const separated = []
+
+            //si lengthFomat=0, pas de formatage
+            if (!this.lengthFormat) this.lengthFormat = splittedVal.length
+
+            for (let i = 0; i < splittedVal.length; i += this.lengthFormat) {
+                separated.push(splittedVal.slice(i, i + this.lengthFormat))
+            }
+            return separated.join(this.separator)
+        }
+
+        showValue(){
+            document.getElementById(this.displayer).innerText= this.formatValue()
+        }
+
+        startLive() {
+            document.querySelectorAll(`#${this.id} input`).forEach(input => {
+                input.addEventListener('input', () => {
+                    this.validate()
+                    this.showValue()
+                })
+            })
+        }
     }
 
-    // function capitalize()
-
-    const elements= {
-        name: 'cardHolderName',
-        number: 'cardNumber',
-        date: 'monthAndYear',
-        cvc: 'cvcForm'
+    const elements = {
+        name: new FormControl(['Jane Appleseed'],'cardHolderName', 'name', true, true, false),
+        number: new FormControl(['0000000000000000'],'cardNumber', 'id', true, false, true, {quantifNumOnly: '{16}', lengthFormat: 4, separator: ' '}),
+        date: new FormControl([new Date().getMonth() + 1, new Date().getFullYear().toString().slice(2)], 'monthAndYear', 'dateExpShow', true, false, true, {quantifNumOnly: '{2}', lengthFormat: 2, separator: '/'}),
+        cvc: new FormControl(['123'],'cvcForm', 'cvc', true, false, true, {quantifNumOnly: '{3}', lengthFormat: 3, separator: ''})
     }
 
-    containAlphaOnly(elements.name)
+    return {
+        validateEveryInput(){
+            let valid= true
+            for (const form in elements){
+                const currIsValid= elements[form].validate()
+                valid = valid && currIsValid
+            }
+
+            return valid
+        },
+        start: () => {
+            for (const form in elements)
+                elements[form].startLive()
+        }
+    }
 })()
+
+liveEditing.start()
