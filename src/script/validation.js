eacb5cce110155a97e6aa2906aa13a64853fff40
Tolotra <tolotramandresy39@gmail.com>
2022-12-30 20:13:42 +0300

    min max validation ok

--- src/script/validation.js
+++ src/script/validation.js
@@ -4,35 +4,40 @@ const liveEditing = (() => {
     }
 
     class FormControl {
-        static verify(formControl, regExp, msg) {
+        static verifyRegex(formControl, regExp, msg) {
             const inputs = document.querySelectorAll(`#${formControl.id} input`)
-            const errorMsgElement = document.querySelector(`#${formControl.id} small`)
             let isValid = true
 
             for (let i = 0; i < inputs.length; i++) {
-                const background = document.querySelector(`#${formControl.id} .gradient:nth-of-type(${i + 1})`)
                 const value = inputs[i].value
+                let currentIsValid= regExp.test(value)
+                isValid = isValid && currentIsValid
 
-                isValid = isValid && regExp.test(value)
+                FormControl.toggleErrorMsg(isValid, msg, formControl, i, currentIsValid)
                 formControl.value[i] = value
 
-                if (!isValid) {
-                    errorMsgElement.innerText = msg
-                    background.classList.add('error')
-                    break
-                }
-                else {
-                    errorMsgElement.innerText = ""
-                    background.classList.remove('error')
-                }
+                // if(!isValid) break
             }
             
             return isValid
         }
 
+        static toggleErrorMsg(isValid, msg, formControl, inputIndex, currentIsValid){
+            const errorMsgElement = document.querySelector(`#${formControl.id} small`)
+            const background = document.querySelector(`#${formControl.id} .gradient:nth-of-type(${inputIndex + 1})`)
+
+            //raha efa correct le eo am input ray de tsy asina backround mena
+            if(currentIsValid) background.classList.remove('error')
+            else background.classList.add('error')
+
+            //raha tsy correct ny ray de apotra ny msg
+            if (!isValid) errorMsgElement.innerText = msg
+            else errorMsgElement.innerText = ""
+        }
+
         static containAlphaOnly(formControl) {
             return function () {
-                return FormControl.verify(formControl, /^[A-Za-z]+(?: [A-Za-z]+)*$/, 'Wrong format, alphabetic only')
+                return FormControl.verifyRegex(formControl, /^[A-Za-z]+(?: [A-Za-z]+)*$/, 'Wrong format, alphabetic only')
             }
         }
 
@@ -40,18 +45,72 @@ const liveEditing = (() => {
             // const minLength=  quantificateur.match(/\d+/)[0] 
             const regExp = new RegExp(`^\\d${quantificateur}$`)
             return function () {
-                return FormControl.verify(formControl, regExp, `Wrong format, number only`)
-                // return FormControl.verify(formControl, regExp, `Wrong format, ${minLength} digits number only`)
+                return FormControl.verifyRegex(formControl, regExp, `Wrong format, number only`)
+                // return FormControl.verifyRegex(formControl, regExp, `Wrong format, ${minLength} digits number only`)
             }
         }
 
-        static isNotEmpty(formControl) {
-            return function () {
-                return FormControl.verify(formControl, /^.+$/, 'Can\'t be blank')
+        static cantBeBlank(formControl){
+            return ()=>{
+                const inputs = document.querySelectorAll(`#${formControl.id} input`)
+                let isValid = true
+
+                for(let i=0; i<inputs.length; i++){
+                    let currentIsValid= true
+                    const value= inputs[i].value
+
+                    if(value.trim()=='')
+                        currentIsValid= false
+
+                    isValid= isValid && currentIsValid
+                    formControl.value[i] = value
+                    FormControl.toggleErrorMsg(isValid, 'Can\'t be blank', formControl, i, currentIsValid)
+                }
+
+                return isValid
+            }
+
+        }
+
+        static checkMinAndMax(formControl, minAndMax){
+            return ()=>{
+                const inputs = document.querySelectorAll(`#${formControl.id} input`)
+                let isValid = true
+    
+                for(let i = 0; i < inputs.length; i++){
+                    const value= inputs[i].value
+                    const minMaxOfCurrent= minAndMax[i]
+                    let currentIsValid= true
+    
+                    if(minMaxOfCurrent){
+                        const min= parseInt(minMaxOfCurrent.min)
+                        const max= parseInt(minMaxOfCurrent.max)
+                        if( min != -Infinity) {
+                            if( value < minMaxOfCurrent.min ){
+                                currentIsValid= false
+                                isValid= isValid && currentIsValid
+                            }
+                            FormControl.toggleErrorMsg(isValid, `Minimum value is ${minMaxOfCurrent.min}`, formControl, i, currentIsValid)
+                        }
+
+                        if(!isValid) continue
+    
+                        if ( max != Infinity ) {
+                            if( value > max ){
+                                currentIsValid= false
+                                isValid= isValid && currentIsValid
+                            }
+                            FormControl.toggleErrorMsg(isValid, `Maximum value is ${minMaxOfCurrent.max}`, formControl, i, currentIsValid)
+                        }
+                    }
+                }
+    
+                return isValid
             }
         }
 
-        constructor(defaultVal, formControl, displayer, cantBeEmpty, alphaOnly, numOnly, options= {quantifNumOnly: '+', lengthFormat: 0, separator: ''}) {
+        //minAndMax= [{min: ..., max: ...}, ....] or null
+        constructor(defaultVal, formControl, displayer, canBeEmpty, alphaOnly, numOnly, options= {quantifNumOnly: '+', lengthFormat: 0, separator: '', minAndMax: []}) {
             this.defaultVal= defaultVal
             this.id = formControl
             this.displayer = displayer
@@ -60,17 +119,22 @@ const liveEditing = (() => {
             this.separator = options.separator
             this.lengthFormat = options.lengthFormat
 
-            if (cantBeEmpty) this.verification.push(FormControl.isNotEmpty(this))
-            if (alphaOnly) this.verification.push(FormControl.containAlphaOnly(this))
-            if (numOnly) this.verification.push(FormControl.containNumOnly(this, options.quantifNumOnly))
+            if (!canBeEmpty) this.verification.push(FormControl.cantBeBlank(this))
+            if (alphaOnly) this.verification.push(FormControl.containAlphaOnly(this,canBeEmpty))
+            if (numOnly) {
+                this.verification.push(FormControl.containNumOnly(this, options.quantifNumOnly, canBeEmpty))
+                if (options.minAndMax) this.verification.push(FormControl.checkMinAndMax(this, options.minAndMax))
+            }
         }
 
         validate() {
             let valid= true
             for (const verif of this.verification){
-                valid= valid && verif()
+                const currentIsValid= verif()
+                valid= valid && currentIsValid
+                if(!valid) return false
             }
-            return valid
+            return true
         }
 
         formatValue() {
@@ -104,11 +168,22 @@ const liveEditing = (() => {
         }
     }
 
+    const d= new Date()
+    const monthAndYear= [d.getMonth() + 1, d.getFullYear().toString().slice(2)];
+    const dateOption= {
+        quantifNumOnly: '{2}', 
+        lengthFormat: 2, 
+        separator: '/', 
+        minAndMax: [
+            { min: '01', max: '12'}
+        ]
+    }
+
     const elements = {
-        name: new FormControl(['Jane Appleseed'],'cardHolderName', 'name', true, true, false),
-        number: new FormControl(['0000000000000000'],'cardNumber', 'id', true, false, true, {quantifNumOnly: '{16}', lengthFormat: 4, separator: ' '}),
-        date: new FormControl([new Date().getMonth() + 1, new Date().getFullYear().toString().slice(2)], 'monthAndYear', 'dateExpShow', true, false, true, {quantifNumOnly: '{2}', lengthFormat: 2, separator: '/'}),
-        cvc: new FormControl(['123'],'cvcForm', 'cvc', true, false, true, {quantifNumOnly: '{3}', lengthFormat: 3, separator: ''})
+        name: new FormControl(['Jane Appleseed'],'cardHolderName', 'name', false, true, false),
+        number: new FormControl(['0000000000000000'],'cardNumber', 'id', false, false, true, {quantifNumOnly: '{16}', lengthFormat: 4, separator: ' '}),
+        date: new FormControl(monthAndYear, 'monthAndYear', 'dateExpShow', false, false, true, dateOption),
+        cvc: new FormControl(['123'],'cvcForm', 'cvc', false, false, true, {quantifNumOnly: '{3}', lengthFormat: 3, separator: ''})
     }
 
     return {
@@ -118,7 +193,7 @@ const liveEditing = (() => {
                 const currIsValid= elements[form].validate()
                 valid = valid && currIsValid
             }
-
+            
             return valid
         },
         start: () => {
--- style-guide.md
+++ style-guide.md
@@ -1,14 +1,6 @@
 # Front-end Style Guide
 
-## Layout
-
-The designs were created to the following widths:
-
-- Mobile: 375px
-- Desktop: 1440px
-
 ## Colors
-
 ### Primary
 
 - Linear gradient (active input border): hsl(249, 99%, 64%) to hsl(278, 94%, 30%)
